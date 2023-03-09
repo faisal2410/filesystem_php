3752a55cd7d50b4fa9dc83d42b58a16f5b500907
Faisal ahmed <faisal2410@yahoo.com>
2023-03-09 09:46:12 +0600

    profile_project updated

--- projects/profile_project/display.php
+++ projects/profile_project/display.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>User Information</title>
+	<!-- Adding Bootstrap CSS -->
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 	<style>
 		table, th, td {
 			border: 1px solid black;
@@ -11,30 +13,38 @@
 	</style>
 </head>
 <body>
-	<h2>User Information</h2>
-	<table>
-		<tr>
-			<th>Name</th>
-			<th>Email</th>
-			<th>Profile Picture</th>
-		</tr>
+	<div class="container">
+		<h2>User Information</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Profile Picture</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				if (($handle = fopen("users.csv", "r")) !== FALSE) {
+					while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+						echo "<tr>";
+						echo "<td>" . $data[0] . "</td>";
+						echo "<td>" . $data[1] . "</td>";
+						echo "<td><img src='" . $data[2] . "' height='100'></td>";
+						echo "</tr>";
+					}
+					fclose($handle);
+				}
+				?>
+			</tbody>
+		</table>
 		<?php
-		if (($handle = fopen("users.csv", "r")) !== FALSE) {
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-				echo "<tr>";
-				echo "<td>" . $data[0] . "</td>";
-				echo "<td>" . $data[1] . "</td>";
-				echo "<td><img src='" . $data[2] . "' height='100'></td>";
-				echo "</tr>";
-			}
-			fclose($handle);
+		if(isset($_COOKIE["username"])) {
+			echo "<p>Welcome, " . $_COOKIE["username"] . "!</p>";
 		}
 		?>
-	</table>
-	<?php
-	if(isset($_COOKIE["username"])) {
-		echo "<p>Welcome, " . $_COOKIE["username"] . "!</p>";
-	}
-	?>
+	</div>
+	<!-- Adding Bootstrap JS -->
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- projects/profile_project/index.php
+++ projects/profile_project/index.php
@@ -2,20 +2,35 @@
 <html>
 <head>
 	<title>Registration Form</title>
+	<!-- Adding Bootstrap CSS -->
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-	<h2>Registration Form</h2>
-	<form method="post" action="process.php" enctype="multipart/form-data">
-		<label>Name:</label>
-		<input type="text" name="name"><br><br>
-		<label>Email:</label>
-		<input type="email" name="email"><br><br>
-		<label>Password:</label>
-		<input type="password" name="password"><br><br>
-		<label>Profile Picture:</label>
-		<input type="file" name="profile_pic"><br><br>
-		<input type="submit" value="Submit">
-	</form>
+	<div class="container">
+		<h2>Registration Form</h2>
+		<form method="post" action="process.php" enctype="multipart/form-data">
+			<div class="form-group">
+				<label>Name:</label>
+				<input type="text" name="name" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email:</label>
+				<input type="email" name="email" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Password:</label>
+				<input type="password" name="password" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Profile Picture:</label>
+				<input type="file" name="profile_pic" class="form-control-file">
+			</div>
+			<input type="submit" value="Submit" class="btn btn-primary">
+		</form>
+	</div>
+	<!-- Adding Bootstrap JS -->
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
+
 
